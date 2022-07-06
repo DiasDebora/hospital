51fef5dbc36b2f184f1c57322965a800f212c730
jose <josermerlin@gmail.com>
2022-07-06 08:30:38 -0300

    Mapeamento OR.

---
+++ src/main/java/modelo/Tutor.java
@@ -0,0 +1,136 @@
+package modelo;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Tutor implements Serializable  {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column (length = 30)
+    private String nome;
+    
+    @Column (length = 30)
+    private String cpf;
+    
+    @Column (length = 50)
+    private String endereco;
+    
+    @Column (length = 30)
+    private String telefone; 
+    
+    @OneToMany (mappedBy = "tutor",
+            fetch = FetchType.EAGER,
+            cascade = CascadeType.MERGE)
+    private List<Animal> animais; 
+    
+ 
+    public Tutor() {
+    }
+
+    public Tutor(Integer id, String nome, String cpf, String endereco, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public List<Animal> getAnimais() {
+        return animais;
+    }
+
+    public void setAnimais(List<Animal> animais) {
+        this.animais = animais;
+    }
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.id);
+        hash = 71 * hash + Objects.hashCode(this.nome);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Tutor other = (Tutor) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Tutor{" + "id=" + id + ", nome=" + nome + '}';
+    }
+    
+    
+    
+}
--- src/test/java/dao/DaoTest.java
+++ src/test/java/dao/DaoTest.java
@@ -1,5 +1,8 @@
 package dao;
 
+import java.util.List;
+import modelo.Animal;
+import modelo.Tutor;
 import modelo.Usuario;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
@@ -8,24 +11,23 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-
 public class DaoTest {
-    
+
     public DaoTest() {
     }
-    
+
     @BeforeAll
     public static void setUpClass() {
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
     }
-    
+
     @BeforeEach
     public void setUp() {
     }
-    
+
     @AfterEach
     public void tearDown() {
     }
@@ -39,5 +41,43 @@ public class DaoTest {
 //        Dao<Usuario> dao = new Dao(Usuario.class);
 //        dao.inserir(u);
 //    }
-//    
+    
+//    @Test
+//    public void testInserirTutor() {
+//        Tutor t = new Tutor();
+//        t.setNome("Zezinho");
+//        t.setCpf("048.554.25658");
+//        t.setEndereco("Rua dos Cravos, 0 - Bandeirantes");
+//        t.setTelefone("43-555-3911");
+//        Dao<Tutor> dao = new Dao(Tutor.class);
+//        dao.inserir(t);
+//    }
+    
+//    @Test
+//    public void testInserirAnimal(){ 
+//        Animal a = new Animal();
+//        a.setEspecie("gato");
+//        a.setRaca("vira-lata");
+//        a.setNome("Mix");
+//        a.setIdade(5);
+//        Dao<Tutor> daoTutor = new Dao(Tutor.class);
+//        Tutor t = daoTutor.buscarPorId(1);
+//        a.setTutor(t);
+//        
+//        Dao<Animal> daoAnimal = new Dao(Animal.class);
+//        daoAnimal.inserir(a);        
+//    }
+    
+    
+//    public void testListarTutor(){
+//        Dao<Tutor> daoTutor = new Dao(Tutor.class);
+//        List<Tutor> lista = daoTutor.listarTodos();
+//        System.out.println("\n\n\n");
+//        for(Tutor t : lista){
+//            System.out.println(t.getNome());
+//            for (Animal a : t.getAnimais()){
+//                System.out.println("     " + a.getNome());
+//            }
+//        }
+//    }
 }
