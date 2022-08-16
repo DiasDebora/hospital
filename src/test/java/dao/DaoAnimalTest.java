c2f62e7cf68b5e48c7697fb866b9669566b724cd
jose <josermerlin@gmail.com>
2022-08-16 17:20:44 -0300

    DaoAnimal

---
+++ src/test/java/dao/DaoAnimalTest.java
@@ -0,0 +1,40 @@
+package dao;
+
+import java.util.List;
+import modelo.Animal;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DaoAnimalTest {
+    
+    public DaoAnimalTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+//    @Test
+//    public void testBuscarPorTutor() {
+//        DaoAnimal dao = new DaoAnimal();
+//        List<Animal> lista = dao.buscarPorTutor(5);
+//        lista.forEach(e -> System.out.println(e.getNome()));
+//    }
+    
+}
